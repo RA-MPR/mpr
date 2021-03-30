aaf97b8221652ffc9854c696bedeeed784068279
Petr Freyburg <freyburgml@gmail.com>
2021-03-30 09:49:59 +0200

    fix contact edit

--- backend/contact/serializers.py
+++ backend/contact/serializers.py
@@ -15,3 +15,14 @@ class ContactSerializer(serializers.ModelSerializer):
     def update(self, contact: models.Contact, validated_data: dict):
         contact.update(validated_data)
         return contact
+
+    def get_fields(self, *args, **kwargs):
+        fields = super(ContactSerializer, self).get_fields(*args, **kwargs)
+        request = self.context.get("request", None)
+        if request and getattr(request, "method", None) == "PUT":
+            fields['id'].required = False
+            fields['name'].required = False
+            fields['surname'].required = False
+            fields['phone'].required = False
+            fields['email'].required = False
+        return fields
