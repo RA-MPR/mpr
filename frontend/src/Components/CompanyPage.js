ecedc324d833802420603efe6f5b8eb2d0e85306
RichardSchleger <schleger13@gmail.com>
2021-04-01 08:36:40 +0200

    Show company detail on row click

--- frontend/src/Components/CompanyPage.js
+++ frontend/src/Components/CompanyPage.js
@@ -6,8 +6,11 @@ import CompanyList from "./CompanyList/CompanyList";
 // eslint-disable-next-line
 import CompanyDetails from "./CompanyDetails/CompanyDetails";
 import "./CompanyPage.css";
+import { useEffect, useRef, useState } from "react";
 
 function CompanyPage() {
+
+  const [detailIco, setDetailIco] = useState("");
 
   const showNewCompanyForm = () =>{
     document.getElementById("companyList").classList.remove("show");
@@ -16,12 +19,25 @@ function CompanyPage() {
 
   const showCompanyList = () =>{
     document.getElementById("companyNew").classList.remove("show");
+    document.getElementById("companyDetail").classList.remove("show");
     document.getElementById("companyList").classList.add("show");
   }
 
-  const showCompanyDetail = (id) =>{
-    console.log("TODO Show company " + id + " detail");
+  const showCompanyDetail = (ico) =>{
+    setDetailIco(ico);
   }
+
+  const isMounted = useRef(false);
+  useEffect(() => {
+    if(isMounted.current){
+      document.getElementById("companyList").classList.remove("show");
+      document.getElementById("companyDetail").classList.add("show");
+    }else{
+      isMounted.current = true;
+    }
+  }, [detailIco]);
+
+
 
   return (
     <div className="root">
@@ -30,6 +46,7 @@ function CompanyPage() {
           <Paper style={{ padding: 16 }}>
             <div className="company-main-screen">
               <CompanyNew onCloseForm={showCompanyList} onShowCompanyDetail={showCompanyDetail} className="company-module"/>
+              <CompanyDetails ico={detailIco} className="company-module" onClose={showCompanyList}/>
               <CompanyList onAddCompany={showNewCompanyForm} onShowCompanyDetail={showCompanyDetail} className="company-module show"/>
             </div>
           </Paper>
