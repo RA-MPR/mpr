5c70fec8adf4156a6e504122f0cfaefb6c7fa891
Petr Freyburg <freyburgml@gmail.com>
2021-04-12 13:42:28 +0200

    backend contact-order

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
 
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from order.models import Order, Month
 from .models import Contact
 from .serializers.common import ContactGetSerializer, ContactSerializer
-
+from rest_framework.response import Response
+from django.db.models import Sum
 
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
@@ -11,3 +16,22 @@ class ContactViewSet(viewsets.ModelViewSet):
         if self.request.method == "GET":
             return ContactGetSerializer
         return ContactSerializer
+
+    @action(methods=['get'], detail=True)
+    def orders(self, request, pk):
+        contact = get_object_or_404(Contact, pk=pk)
+        company_ico = contact.company.ico
+
+        summary = (Order.objects
+                   .filter(company=company_ico)
+                   .annotate(month=Month('date'))
+                   .values('month')
+                   .annotate(total=Sum('sum'))
+                   .order_by())
+
+        return Response(
+           summary,
+            status=status.HTTP_200_OK,
+        )
+
+
